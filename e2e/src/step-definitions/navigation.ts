affee32cfaf4c64d51a34122882a4ecdf4865b4f
Hoon Wee <mrgravity817@gmail.com>
2023-04-18 21:25:18 +0900

    feat: add given step for navigating into home page

---
+++ e2e/src/step-definitions/navigation.ts
@@ -0,0 +1,6 @@
+import { Given } from "@cucumber/cucumber"
+
+Given(/^I am on the home page$/, async () => {
+  console.log("I am on the home page") // just to check if regex pattern is matched
+  await global.page.goto("http://localhost:3000/")
+})
