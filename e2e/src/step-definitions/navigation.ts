73ce2fd2ada3a03da94f6394b67bcb474d52ad88
Hoon Wee <mrgravity817@gmail.com>
2023-04-23 16:50:48 +0900

    test: add regex pattern to match web pages

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -1,6 +1,6 @@
 import { Given } from "@cucumber/cucumber"
 
-Given(/^I am on the home page$/, async () => {
+Given(/^I am on the "([^"]*)" page$/, async () => {
   console.log("I am on the home page") // just to check if regex pattern is matched
   await global.page.goto("http://localhost:3000/")
 })
