478d41e71e9029d8ceb5e41dc6fdcef57814ce72
Hoon Wee <mrgravity817@gmail.com>
2023-04-23 16:52:47 +0900

    test: get page name from callback

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -1,6 +1,6 @@
 import { Given } from "@cucumber/cucumber"
 
-Given(/^I am on the "([^"]*)" page$/, async () => {
-  console.log("I am on the home page") // just to check if regex pattern is matched
+Given(/^I am on the "([^"]*)" page$/, async (pageId: string) => {
+  console.log(`I am on the ${pageId} page`) // just to check if regex pattern is matched
   await global.page.goto("http://localhost:3000/")
 })
