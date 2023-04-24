54557250932c6dbd81a3063d4250c7ef70df8e63
Hoon Wee <mrgravity817@gmail.com>
2023-04-25 00:31:36 +0900

    docs: add typing for `global.page`

---
+++ e2e/src/index.d.ts
@@ -0,0 +1,5 @@
+import { Page } from "@playwright/test";
+
+declare global {
+  var page: Page;
+}
--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -1,6 +1,6 @@
-import { Given } from "@cucumber/cucumber"
+import { Given } from "@cucumber/cucumber";
 
 Given(/^I am on the "([^"]*)" page$/, async (pageId: string) => {
-  console.log(`I am on the ${pageId} page`) // just to check if regex pattern is matched
-  await global.page.goto("http://localhost:3000/")
-})
+  console.log(`I am on the ${pageId} page`); // just to check if regex pattern is matched
+  await global.page.goto("http://localhost:3000/");
+});
