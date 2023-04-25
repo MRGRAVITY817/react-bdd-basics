304c5cbd9c95d0c91f2be4af0ab8ef9307360686
Hoon Wee <mrgravity817@gmail.com>
2023-04-26 02:13:48 +0900

    fix: navigation.ts to use lexical this

--- e2e/src/step-definitions/navigation.ts
+++ e2e/src/step-definitions/navigation.ts
@@ -1,6 +1,11 @@
 import { Given } from "@cucumber/cucumber";
 
-Given(/^I am on the "([^"]*)" page$/, async (pageId: string) => {
+Given(/^I am on the "([^"]*)" page$/, async function (pageId: string) {
+  const {
+    screen: { page },
+  } = this;
+
   console.log(`I am on the ${pageId} page`); // just to check if regex pattern is matched
-  await global.page.goto("http://localhost:3000/");
+
+  await page.goto("http://localhost:3000/");
 });
