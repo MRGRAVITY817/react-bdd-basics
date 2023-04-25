b9cb403a4a39c6e0c77a89d52e06f5580d3aa705
Hoon Wee <mrgravity817@gmail.com>
2023-04-26 02:07:28 +0900

    fix: remove BeforeAll and AfterAll

--- e2e/src/step-definitions/setup/hooks.ts
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -1,28 +1,17 @@
 import { BeforeAll, Before, AfterAll, After } from "@cucumber/cucumber";
-const { chromium } = require("playwright");
+import { ScenarioWorld } from "./world";
 
-BeforeAll(async () => {
-  global.browser = await chromium.launch({
-    // Don't show the browser (essential for running test in CI, or GUI-less env)
-    headless: true,
-  });
-});
-
-AfterAll(async () => {
-  await global.browser.close();
-});
-
-Before(async (scenario) => {
-  // `newContext()` creates completely new browser environment (same as incognito)
-  global.context = await global.browser.newContext({
+Before(async function (this: ScenarioWorld, scenario) {
+  const contextOptions = {
     recordVideo: {
       dir: "./reports/videos/" + scenario.pickle.name,
     },
-  });
-  global.page = await global.context.newPage();
+  };
+
+  const ready = await this.init(contextOptions);
 });
 
-After(async (scenario) => {
+After(async function (this: ScenarioWorld, scenario) {
   const scenarioStatus = scenario.result?.status;
   if (scenarioStatus === "FAILED") {
     await global.page.screenshot({
